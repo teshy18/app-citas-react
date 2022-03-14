191463d653723968f66c2a91e9e1dc52d479f31f
Sebastian <68450501+teshy18@users.noreply.github.com>
2022-03-14 19:03:55 -0300

    se agregaron estilos con tailwind

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <h2>Formulario</h2>
+    <div className='md:w-1/2 lg:w-2/5'>
+
+      <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
+
+    </div>
   )
 }
 
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ListadoPacientes = () => {
   return (
+    <div className='md:w-1/2 lg:w-3/5'>
+    
     <h2>Listado Pacientes</h2>
+      
+    </div>
   )
 }
 
