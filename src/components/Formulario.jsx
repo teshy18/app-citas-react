0ed21d61a4450ceeca95f722f980ba11b8fc0b84
Sebastian <68450501+teshy18@users.noreply.github.com>
2022-03-17 01:12:46 -0300

    Se completo el formulario  y se agregaron estilos con Tailwind

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,7 +4,82 @@ const Formulario = () => {
   return (
     <div className='md:w-1/2 lg:w-2/5'>
 
-      <h2 className='font-black text-3xl text-center'>Seguimiento Pacientes</h2>
+      <h2 className='font-black text-3xl text-center'>
+        Seguimiento Pacientes
+      </h2>
+
+      <p className='text-lg mt-5 text-center mb-10'>
+          Añade Pacientes y {''}
+          <span className='text-indigo-600 font-bold'>Administralos</span>
+      </p>
+
+      <form className='bg-white shadow-md rounded-lg py-10 px-5 mb-10'>
+        
+        <div className='mb-5'>
+          <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>
+            Nombre Mascota
+          </label>
+          <input
+            id='mascota'
+            type="text"
+            placeholder='Escribe el nombre de la mascota'
+            className='w-full border-2 p-2 mt-2 placeholder-gray-400 rounded-md'
+          />
+        </div>
+      
+        <div className='mb-5'>
+          <label htmlFor='dueño' className='block text-gray-700 uppercase font-bold'>
+            Nombre Dueño
+          </label>
+          <input
+            id='dueño'
+            type="text"
+            placeholder='Escribe el nombre del dueño'
+            className='w-full border-2 p-2 mt-2 placeholder-gray-400 rounded-md'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='email' className='block text-gray-700 uppercase font-bold'>
+            Email
+          </label>
+          <input
+            id='email'
+            type="email"
+            placeholder='Escribe un email de contacto'
+            className='w-full border-2 p-2 mt-2 placeholder-gray-400 rounded-md'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='ingreso' className='block text-gray-700 uppercase font-bold'>
+            Ingreso
+          </label>
+          <input
+            id='ingreso'
+            type="date"
+            className='w-full border-2 p-2 mt-2 placeholder-gray-400 rounded-md'
+          />
+        </div>
+
+        <div className='mb-5'>
+          <label htmlFor='comentario' className='block text-gray-700 uppercase font-bold'>
+            Comentario
+          </label>
+          <textarea
+            id='comentario'
+            className='w-full border-2 p-2 mt-2 placeholder-gray-400 rounded-md'
+            placeholder='Escribe un comentario'
+          />
+        </div>
+
+        <input
+          type="submit"
+          className='bg-indigo-600 p-3 w-full font-bold rounded-md text-white cursor-pointer hover:bg-indigo-700 transition-colors'
+          value="Agregar Paciente"
+        />
+
+      </form>
 
     </div>
   )
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,40 @@
+
+const Paciente = () => {
+  return (
+    <div className='bg-white shadow-md rounded-lg py-10 px-5 m-3'>
+        
+        <p className='font-bold text-gray-700 uppercase mb-3'> Nombre: {''}
+          <span className='font-normal normal-case'>
+              Hook
+          </span>
+        </p>
+
+        <p className='font-bold text-gray-700 uppercase mb-3'> Dueño: {''}
+          <span className='font-normal normal-case'>
+              Hook
+          </span>
+        </p>
+        
+        <p className='font-bold text-gray-700 uppercase mb-3'> Email: {''}
+          <span className='font-normal normal-case'>
+              Hook
+          </span>
+        </p>
+        
+        <p className='font-bold text-gray-700 uppercase mb-3'> Ingreso: {''}
+          <span className='font-normal normal-case'>
+              Hook
+          </span>
+        </p>
+
+        
+        <p className='font-bold text-gray-700 uppercase mb-3'> Comentario: {''}
+          <span className='font-normal normal-case'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+          </span>
+        </p>
+      </div>
+  )
+}
+
+export default Paciente
