6fc1156115ac5eb32447ef2bbb9bfe6f77cb38ba
Sebastian <68450501+teshy18@users.noreply.github.com>
2022-03-20 03:14:48 -0300

    los cambios en la lista de pacientes ahora se almacena en localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -6,6 +6,19 @@ import ListadoPacientes from "./components/ListadoPacientes"
 function App() {
   const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})
+
+  useEffect(()=>{
+    const obtenerLS = ()=> {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS)
+    }
+    obtenerLS();
+  },[]);
+
+  useEffect(()=>{
+    localStorage.setItem('pacientes', JSON.stringify( pacientes))
+  },[pacientes])
+
 
   const eliminarPaciente = (id)=>{
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
